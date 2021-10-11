fb28effa8b7f3fc6428668554b219674b6f37d9c
DenuccioDiego <diegodenuccio95@gmail.com>
2021-10-11 15:31:41 +0200

    richiesta dati utente

---
+++ main.js
@@ -0,0 +1,47 @@
+// Prima condizine
+let distanza = prompt('Quanti km vuoi percorrere?');
+
+if(isNaN(distanza)){
+     alert('Occhio devi inserire un numero');
+}
+
+
+// Seconda condizione
+let età = Number(prompt('Qual\'é l\'etá del viaggiatore?'));
+
+if(isNaN(età)){
+     alert('Occhio devi inserire un numero');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
