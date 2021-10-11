b4b7c70f9dbea25c17bac93cf6867bfe5934e15b
DenuccioDiego <diegodenuccio95@gmail.com>
2021-10-11 15:55:00 +0200

    prezzo_finale

--- main.js
+++ main.js
@@ -1,31 +1,44 @@
+
+// ---------//
 // Prima condizine
+// ---------//
 let distanza = prompt('Quanti km vuoi percorrere?');
 
 if(isNaN(distanza)){
      alert('Occhio devi inserire un numero');
 }
 
-
+// ---------//
 // Seconda condizione
+// ---------//
 let età = Number(prompt('Qual\'é l\'etá del viaggiatore?'));
 
 if(isNaN(età)){
      alert('Occhio devi inserire un numero');
 }
 
+// ---------//
+// calcolo prezzo biglietto
+// ---------//
 
 let prezzoBase = distanza * 0.21;
 
 if (età < 18){
-     let prezzoScontatoUnder18 = prezzoBase - (prezzoBase * 0.20)
+     var prezzoFinale = prezzoBase - (prezzoBase * 0.20);
      
 }
 
+if (età > 65){
+     var prezzoFinale = prezzoBase - (prezzoBase * 0.40);
 
+}
 
+else {
+     var prezzoFinale = prezzoBase ;
 
+}
 
-
+document.getElementById("prezzo_biglietto").innerHTML = prezzoFinale;
 
 
 
